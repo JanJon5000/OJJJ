c369f229a66ef324dbd7b975c190e973bea7d763
JanJon5000 <jan@ipoczta.pl>
2023-10-31 16:05:21 +0100

    minor changes to how objects on the HTML files are filtered

--- blok/views.py
+++ blok/views.py
@@ -3,16 +3,16 @@ from django.utils import timezone
 from .models import Password, Site, File, Photo
 
 def file_list(request):
-    files = File.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    files = File.objects
     return render(request, 'blok/file_list.html', {})
 
 def site_list(request):
-    #sites = Site.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    sites = Site.objects
     return render(request, 'blok/site_list.html', {})
 
 def photo_list(request):
-    #photos = Photo.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    photos = Photo.objects
     return render(request, 'blok/photo_list.html', {})
 
 def password(request):
-    return render(request, 'blok/haslo.html', {})
+    return render(request, 'blok/password.html', {})
