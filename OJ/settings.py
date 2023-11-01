037d763e7d47f213ba84c4aead86a05be2c98ec6
JanJon5000 <jan@ipoczta.pl>
2023-11-01 18:06:33 +0100

    bug fixes

--- OJ/settings.py
+++ OJ/settings.py
@@ -125,6 +125,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+CSRF_TRUSTED_ORIGINS = ['https://ojjj.azurewebsites.net']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = Path(BASE_DIR / 'media')
