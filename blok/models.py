6dea8407eb1c9f99da7747e276b1f33850281991
JanJon5000 <jan@ipoczta.pl>
2023-11-11 02:02:55 +0100

    icon ,static files, categories

--- blok/models.py
+++ blok/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 from django.utils import timezone
+from django import forms
+
 
 
 class Site(models.Model):
     URL_link = models.URLField()
     name = models.TextField()
+    PossibleTags = (
+        ("Szkola", "Szkola"),
+        ("Osobiste", 'Osobiste'),
+        ("Inne", "Inne")
+    )
 
+    tag = models.CharField(choices=PossibleTags, default="Szkola", max_length=10)
+    
     def __str__(self):
         return self.name
 
